93daa38e62f2fbf87346318df15e95d76c3cdc39
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-06-01 16:22:02 -0300

    Some  bug fix

--- index.js
+++ index.js
@@ -10,20 +10,16 @@ let requestOptions = {
 };
 
 fetch(
-  $(
-    `
+  (`
   https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}`,
-    requestOptions
-  )
+  requestOptions)
 )
   .then((response) => response.json())
   .then((result) => {
     Object.keys(result.rate).forEach((moneda) => {
-
-    }
-  )})
+      $("from-currency").append($(`<option>${moneda}>`));
+    });
+  })
   .catch((error) => console.log("error", error));
 
-const fromCurrencyList = $("from-currency");
-
-for (let i = 0; i< )
+//const fromCurrencyList = $("from-currency");
