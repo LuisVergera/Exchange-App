0c0f98d2fcd0ae11d34204d858549efc384b580b
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-06-01 17:01:39 -0300

    New inputs

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /// <reference types="jquery" />
+const selectButton = $("select");
+const fromCurrency = $("from-currency");
+const toCurrency = $("to-Currency");
 
 let myHeaders = new Headers();
 myHeaders.append("apikey", "LGpPW5F47olBdyLHC1yEvAZFk0WZf2w3");
@@ -23,3 +26,16 @@ fetch(
   .catch((error) => console.log("error", error));
 
 //const fromCurrencyList = $("from-currency");
+
+fetch(
+  (`
+  https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}`,
+  requestOptions)
+)
+  .then((response) => response.json())
+  .then((result) => {
+    Object.keys(result.rate).forEach((moneda) => {
+      $("from-currency").append($(`<option>${moneda}>`));
+    });
+  })
+  .catch((error) => console.log("error", error));
