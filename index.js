2c66fc12bd0d895563fd46fc8706234191fe3cd4
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-05-31 22:32:59 -0300

    Added Fetch from api and date selection

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 /// <reference types="jquery" />
 
-const key = "LGpPW5F47olBdyLHC1yEvAZFk0WZf2w3";
+let myHeaders = new Headers();
+myHeaders.append("apikey", "LGpPW5F47olBdyLHC1yEvAZFk0WZf2w3");
+
+let requestOptions = {
+  method: "GET",
+  redirect: "follow",
+  headers: myHeaders,
+};
+
+fetch(
+  $(
+    `
+  https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}`,
+    requestOptions
+  )
+)
+  .then((response) => response.json())
+  .then((result) => {
+    Object.keys(result.rate).forEach((moneda) => {
+
+    }
+  )})
+  .catch((error) => console.log("error", error));
+
+const fromCurrencyList = $("from-currency");
+
+for (let i = 0; i< )
