e8f6e050f81e69b5421eafd4585f1ab3134f4e5f
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-06-02 14:52:15 -0300

    Function design update

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /// <reference types="jquery" />
-
-const selectButton = $("select");
-const fromCurrency = $("from-currency");
-const toCurrency = $("to-Currency");
+const date = $("#date-selector").val();
+const selectButton = $("#select");
+const baseCurrency = $("#base-currency").text();
 
 let myHeaders = new Headers();
 myHeaders.append("apikey", "LGpPW5F47olBdyLHC1yEvAZFk0WZf2w3");
@@ -11,7 +10,7 @@ let requestOptions = {
   method: "GET",
   redirect: "follow",
   headers: myHeaders,
-};
+}; /*
 fetch("https://api.apilayer.com/exchangerates_data/latest", requestOptions)
   .then((response) => response.json())
   .then((result) => {
@@ -22,4 +21,21 @@ fetch("https://api.apilayer.com/exchangerates_data/latest", requestOptions)
         $("ul").append($(`<li>${moneda}: ${result.rates[moneda]}</li>`));
       })
       .catch((error) => console.log("error", error));
-  });
+  });*/
+
+selectButton.click(() => {
+  fetch(
+    `https://api.apilayer.com/exchangerates_data/${date}?base=${baseCurrency}`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      $("ul").html("");
+
+      Object.keys(result.rates)
+        .forEach((moneda) => {
+          $("ul").append($(`<li>${moneda}: ${result.rates[moneda]}</li>`));
+        })
+        .catch((error) => console.log("error", error));
+    });
+});
